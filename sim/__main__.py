78b50bc2efd7e463fc41c3d5cc7e103dad5c5e4c
Jaco Jansen <jaco.jansen@iress.com.au>
2019-06-28 02:12:57 +0100

    basic sim

---
+++ sim/__main__.py
@@ -0,0 +1,81 @@
+from collections import defaultdict
+from itertools import chain
+from math import sqrt
+
+from loguru import logger
+from trueskill import quality_1vs1, Rating, BETA, global_env
+
+from data import DATA
+
+
+def win_probability(team1, team2):
+    delta_mu = sum(r.mu for r in team1) - sum(r.mu for r in team2)
+    sum_sigma = sum(r.sigma ** 2 for r in chain(team1, team2))
+    size = len(team1) + len(team2)
+    denom = sqrt(size * (BETA * BETA) + sum_sigma)
+    ts = global_env()
+    return ts.cdf(delta_mu / denom)
+
+
+def get_implied_odds(american):
+    if american > 0:
+        return 100 / (american + 100)
+    return abs(american) / (abs(american) + 100)
+
+
+def main():
+    ratings = defaultdict(lambda: Rating())
+    balance = 0
+    logger.info(f'Balance is {balance}. good luck!')
+
+    for scene in DATA:
+        logger.info(f'{scene["date"]} {scene["name"]}')
+        for fight in scene['fights']:
+            f1 = fight['fighters'][0]['surname']
+            f2 = fight['fighters'][1]['surname']
+
+            draw_prob = quality_1vs1(ratings[f1], ratings[f2])
+            win1_prob = round(win_probability([ratings[f1]], [ratings[f2]]), 2)
+
+            # get winner
+            fw = fight['winner']['fighter']
+            is_win_1 = fw == f1
+            fl = f2 if is_win_1 else f1
+
+            # absolute betting
+            BET_AMT = 10
+            payout = -BET_AMT
+            f1_odds = fight['odds'][f1]
+            f2_odds = fight['odds'][f2]
+            if is_win_1 and (win1_prob > 0.5 or win1_prob == 0.5 and f1_odds < f2_odds):
+                if f1_odds > 0:
+                    payout += f1_odds + BET_AMT
+                else:
+                    payout += 100 * BET_AMT / abs(f1_odds) + BET_AMT
+            elif not is_win_1 and (win1_prob < 0.5 or win1_prob == 0.5 and f2_odds < f1_odds):
+                if f2_odds > 0:
+                    payout += f2_odds + BET_AMT
+                else:
+                    payout += 100 * BET_AMT / abs(f2_odds) + BET_AMT
+            balance += payout
+
+            logger.info(f'[{win1_prob * 100:.0f}%/{draw_prob * 100:.0f}%] {fw} [{ratings[fw].mu:.2f}] {fight["winner"]["by"]} {fl} [{ratings[fl].mu:.2f}] ==> {payout:.0f} bal:{balance:.0f}')
+
+
+
+def to_decimal_odds(us_odds):
+    if us_odds > 0:
+        return us_odds / 100 + 1
+    else:
+        return 100 / us_odds + 1
+
+
+if __name__ == '__main__':
+    main()
+
+
+# from trueskill import Rating, quality_1vs1, rate_1vs1
+# alice, bob = Rating(25), Rating(30)  # assign Alice and Bob's ratings
+# if quality_1vs1(alice, bob) < 0.50:
+#     print('This match seems to be not so fair')
+# alice, bob = rate_1vs1(alice, bob)  # update the ratings after the match
---
+++ sim/data.py
@@ -0,0 +1,307 @@
+DATA = [
+    {
+        'date': '2019-06-22',
+        'name': 'UFC Fight Night',
+        'location': 'Greenville, South Carolina',
+        'venue': 'Bon Secours Wellness Arena',
+        'fights': [
+            {
+                'time': '16:05',
+                'gender': 'mens',
+                'weight_class': 'middleweight',
+                'fighters': [
+                    {
+                        'name': 'Deron',
+                        'surname': 'WINN',
+                        'stats': '7-1-0',
+                    },
+                    {
+                        'name': 'Eric',
+                        'surname': 'SPICELY',
+                        'stats': '10-5-0',
+                    },
+                ],
+                'winner': {
+                    'fighter': 'WINN',
+                    'by': 'u.dec',
+                    'round': 3,
+                    'time': '5:00',
+                },
+                'odds': {
+                    'SPICELY': 290,
+                    'WINN': -340,
+                }
+            },
+            {
+                'time': '16:35',
+                'gender': 'mens',
+                'weight_class': 'lightweight',
+                'fighters': [
+                    {
+                        'name': 'Matt',
+                        'surname': 'WIMAN',
+                        'stats': '16-7-0',
+                    },
+                    {
+                        'name': 'Luis',
+                        'surname': 'PENA',
+                        'stats': '6-1-0',
+                    },
+                ],
+                'winner': {
+                    'fighter': 'PENA',
+                    'by': 'ko/tko',
+                    'round': 3,
+                    'time': '1:14',
+                },
+                'odds': {
+                    'WIMAN': 285,
+                    'PENA': -352,
+                }
+            },
+            {
+                'time': '17:35',
+                'gender': 'mens',
+                'weight_class': 'featherweight',
+                'fighters': [
+                    {
+                        'name': 'Dan',
+                        'surname': 'IGE',
+                        'stats': '11-2-0',
+                    },
+                    {
+                        'name': 'Kevin',
+                        'surname': 'AGUILAR',
+                        'stats': '0-0-0',
+                    },
+                ],
+                'winner': {
+                    'fighter': 'IGE',
+                    'by': 'u.dec',
+                    'round': 3,
+                    'time': '5:00',
+                },
+                'odds': {
+                    'IGE': 121,
+                    'AGUILAR': -136,
+                }
+            },
+            {
+                'time': '18:35',
+                'gender': 'womans',
+                'weight_class': 'flyweight',
+                'fighters': [
+                    {
+                        'name': 'Ariane',
+                        'surname': 'LIPSKI',
+                        'stats': '11-4-0',
+                    },
+                    {
+                        'name': 'Molly',
+                        'surname': 'MCCANN',
+                        'stats': '8-2-0',
+                    },
+                ],
+                'winner': {
+                    'fighter': 'MCCANN',
+                    'by': 'u.dec',
+                    'round': 3,
+                    'time': '5:00',
+                },
+                'odds': {
+                    'MCCANN': 205,
+                    'LIPSKI': -235,
+                },
+            },
+            {
+                'time': '20:00',
+                'gender': 'womans',
+                'weight_class': 'strawweight',
+                'fighters': [
+                    {
+                        'name': 'Ashley',
+                        'surname': 'YODER',
+                        'stats': '6-4-0',
+                    },
+                    {
+                        'name': 'Syuri',
+                        'surname': 'KONDO',
+                        'stats': '6-2-0',
+                    },
+                ],
+                'winner': {
+                    'fighter': 'YODER',
+                    'by': 'u.dec',
+                    'round': 3,
+                    'time': '5:00',
+                },
+                'odds': {
+                    'KONDO': 120,
+                    'YODER': -135,
+                }
+            },
+            {
+                'time': '20:22',
+                'gender': 'mens',
+                'weight_class': 'bantamweight',
+                'fighters': [
+                    {
+                        'name': 'Andre',
+                        'surname': 'EWELL',
+                        'stats': '13-5-0',
+                    },
+                    {
+                        'name': 'Anderson Berlingeri',
+                        'surname': 'DOS SANTOS',
+                        'stats': '0-0-0',
+                    },
+                ],
+                'winner': {
+                    'fighter': 'EWELL',
+                    'by': 'u.dec',
+                    'round': 3,
+                    'time': '5:00',
+                },
+                'odds': {
+                    'DOS SANTOS': 120,
+                    'EWELL': -135,
+                }
+            },
+            {
+                'time': '20:30',
+                'gender': 'mens',
+                'weight_class': 'middleweight',
+                'fighters': [
+                    {
+                        'name': 'Kevin',
+                        'surname': 'HOLLAND',
+                        'stats': '15-4-0',
+                    },
+                    {
+                        'name': 'Alessio',
+                        'surname': 'DI CHIRICO',
+                        'stats': '12-2-0',
+                    },
+                ],
+                'winner': {
+                    'fighter': 'HOLLAND',
+                    'by': 'u.dec',
+                    'round': 3,
+                    'time': '5:00',
+                },
+                'odds': {
+                    'DI CHIRICO': 186,
+                    'HOLLAND': -212,
+                },
+            },
+            {
+                'time': '20:45',
+                'gender': 'mens',
+                'weight_class': 'welterweight',
+                'fighters': [
+                    {
+                        'name': 'Bryan',
+                        'surname': 'BARBERENA',
+                        'stats': '14-6-0',
+                    },
+                    {
+                        'name': 'Randy',
+                        'surname': 'BROWN',
+                        'stats': '10-3-0',
+                    },
+                ],
+                'winner': {
+                    'fighter': 'BROWN',
+                    'by': 'ko/tko',
+                    'round': 3,
+                    'time': '2:54',
+                },
+                'odds': {
+                    'BROWN': 198,
+                    'BARBERENA': -237,
+                },
+            },
+            {
+                'time': '21:10',
+                'gender': 'mens',
+                'weight_class': 'featherweight',
+                'fighters': [
+                    {
+                        'name': 'Renato',
+                        'surname': 'MOICANO',
+                        'stats': '13-2-1',
+                    },
+                    {
+                        'name': 'Chan Sung',
+                        'surname': 'JUNG',
+                        'stats': '14-5-0',
+                    },
+                ],
+                'winner': {
+                    'fighter': 'JUNG',
+                    'by': 'ko/tko',
+                    'round': 1,
+                    'time': '0:58'
+                },
+                'odds': {
+                    'JUNG': 187,
+                    'MOICANO': -213,
+                },
+            },
+            {
+                'time': '21:30',
+                'gender': 'mens',
+                'weight_class': 'heavyweight',
+                'fighters': [
+                    {
+                        'name': 'Allen',
+                        'surname': 'CROWDER',
+                        'stats': '10-3-0',
+                    },
+                    {
+                        'name': 'Jairzinho',
+                        'surname': 'ROZENSTRUIK',
+                        'stats': '6-0-0',
+                    },
+                ],
+                'winner': {
+                    'fighter': 'ROZENSTRUIK',
+                    'by': 'ko/tko',
+                    'round': 1,
+                    'time': '0:09',
+                },
+                'odds': {
+                    'ROZENSTRUIK': -223,
+                    'CROWDER': 195,
+                },
+            },
+            {
+                'time': '22:00',
+                'gender': 'womans',
+                'weight_class': 'flyweight',
+                'fighters': [
+                    {
+                        'name': 'Andrea',
+                        'surname': 'LEE',
+                        'stats': '10-2-0',
+                    },
+                    {
+                        'name': 'Montana',
+                        'surname': 'DE LA ROSA',
+                        'stats': '10-4-0',
+                    },
+                ],
+                'winner': {
+                    'fighter': 'LEE',
+                    'by': 'u.dec',
+                    'round': 3,
+                    'time': '5:00',
+                },
+                'odds': {
+                    'DE LA ROSA': 150,
+                    'LEE': -170,
+                },
+            },
+        ]
+    }
+]
